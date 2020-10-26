990c6435ef5ca828d953b921f872535154a6db73
michael07062002 <71323994+michael07062002@users.noreply.github.com>
2020-10-27 01:00:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+import re
+from typing import List
+
+f = open("steam_description_data.csv", 'r', encoding='utf8')
+words = 0
+symbols = 0
+symbols_without_space = 0
+symbols_without_comma = 0
+symbols_without_dot = 0
+symbols_without_marks = 0
+symbols_without_exclamatiom_mark = 0
+symbols_without_colon = 0
+comma = 0
+dot = 0
+comma = 0
+exclamatiom_mark = 0
+colon = 0
+sentence = 0
+for line in f:
+    wordlist5 = re.split(r"[,\!]", line)
+    wordlist4 = line.split(':')
+    wordlist3 = line.split('!')
+    wordlist = line.split()
+    wordlist1 = line.split(',')
+    wordlist2 = line.split('.')
+    symbols = symbols + len(line)
+    words = words + len(wordlist)
+    symbols_without_space += sum(len(word) for word in wordlist)
+    symbols_without_comma += sum(len(word) for word in wordlist1)
+    symbols_without_dot += sum(len(word) for word in wordlist2)
+    symbols_without_exclamatiom_mark += sum(len(word) for word in wordlist3)
+    symbols_without_colon += sum(len(word) for word in wordlist4)
+    sentence = sentence + len(wordlist5)
+colon = symbols - symbols_without_colon
+exclamatiom_mark = symbols -symbols_without_exclamatiom_mark
+dot = symbols - symbols_without_dot
+comma = symbols - symbols_without_comma
+symbols_without_marks = symbols - dot - comma - exclamatiom_mark - colon
+print('Кол-во символов без знаков препинания')
+print(symbols_without_marks)
+print('Кол-во слов')
+print(words)
+print('Кол-во символов')
+print(symbols)
+print('Кол-во симоволов без пробелов')
+print(symbols_without_space)
+print('Кол-во предложений')
+print(sentence)
+
